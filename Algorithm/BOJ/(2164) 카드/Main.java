f254b9ff2e0e94e275ba3517a4608c66859bdd0a
Taehyun <ddd2520@naver.com>
2020-01-28 21:28:56 +0900

    카드 - 시뮬레이션

---
+++ Algorithm/BOJ/(2164) 카드/Main.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String... args) throws Exception{
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(bf.readLine());
+        ArrayDeque<Integer> queue = new ArrayDeque();
+        for(int i = 1 ; i <= N ; i++){
+            queue.add(i);
+        }
+        while(queue.size() != 1){
+            ((ArrayDeque) queue).pollFirst();
+            int a = queue.pollFirst();
+            queue.add(a);
+        }
+        System.out.println(queue.getFirst());
+    }
+}
